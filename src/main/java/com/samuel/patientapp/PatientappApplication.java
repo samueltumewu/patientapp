85592ab14f9adbdf896cc73f82098f161f240be6
samueltumewu <samueltumewu21@gmail.com>
2024-08-15 09:24:50 +0700

    fix(*): adjust correct annotations to avoid any entityManager conflicts

--- src/main/java/com/samuel/patientapp/PatientappApplication.java
+++ src/main/java/com/samuel/patientapp/PatientappApplication.java
@@ -1,15 +1,9 @@
 package com.samuel.patientapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
-@Configuration
-@EnableAutoConfiguration
 public class PatientappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/samuel/patientapp/repository/PatientRepository.java
+++ src/main/java/com/samuel/patientapp/repository/PatientRepository.java
@@ -4,7 +4,9 @@ import com.samuel.patientapp.model.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PatientRepository  extends JpaRepository<Patient, Long> {
     public Page<Patient> findByFirstNameIgnoreCaseContainingOrLastNameIgnoreCaseContaining(
             String firstname, String lastname, Pageable pageable);
